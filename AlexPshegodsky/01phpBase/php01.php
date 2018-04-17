94a501ca9eabc00c6255900507ca6ae42b022b96
Alex Pshegodsky <lexkiev@ukr.net>
2018-04-17 22:08:57 +0300

    commit

--- AlexPshegodsky/01phpBase/php01.php
+++ AlexPshegodsky/01phpBase/php01.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 16.04.2018
- * Time: 21:08
- */
+$options = getopt('a::b::c::');
+$r1 = $options['a'];
+$r2 = $options['b'];
+$r3 = $options['c'];
+
+if (!isset($r1, $r2, $r3)) {
+	die('Not enough parameters!');
+}
+
+$result = (($r1 + $r2) * $r3) / ($r1 + $r2 + $r3);
+
+echo $result;
--- AlexPshegodsky/01phpBase/php03.php
+++ AlexPshegodsky/01phpBase/php03.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 17.04.2018
- * Time: 19:58
- */
+$options = getopt('a:');
+$mass = $options['a'];
+
+define("G_EARTH", 9.80665);
+
+
+if ($mass <= 0) {
+	die("Wrong mass value");
+}
+
+$weight = $mass * G_EARTH;
+
+echo "Weight is $weight";
