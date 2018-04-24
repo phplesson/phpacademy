df8b4bf7b0033bae416bad49dc2eda856790f441
Andrey Kaplunenko <me@kaplunenko.name>
2018-04-24 15:26:07 +0300

    added arrays_loops_tasks [01.php-03.php]

---
+++ andrey_kaplunenko/arrays_loops_tasks/02.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 4/24/18
+ * Time: 3:07 PM
+ */
+/*
+ * Дан массив с элементами 1, 20, 15, 17, 24, 35. С помощью цикла foreach найдите сумму элементов этого массива. Запишите ее в переменную $result.
+ * */
+
+$numbers = [1, 20, 15, 17, 24, 35];
+$result = 0;
+
+foreach ($numbers as $value) {
+    $result += $value;
+}
+
+printf ('%d'.PHP_EOL, $result);
---
+++ andrey_kaplunenko/arrays_loops_tasks/03.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 4/24/18
+ * Time: 3:13 PM
+ */
+/*
+ * Дан массив с элементами 26, 17, 136, 12, 79, 15. С помощью цикла foreach найдите сумму квадратов элементов этого массива.
+ * Результат запишите переменную $result.
+ * */
+
+$numbers = [26, 17, 136, 12, 79, 15];
+$result = 0;
+
+foreach ($numbers as $value) {
+    $result += $value**2;
+}
+
+printf ('%d'.PHP_EOL, $result);
