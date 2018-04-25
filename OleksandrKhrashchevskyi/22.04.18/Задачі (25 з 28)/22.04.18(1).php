8e552ea2f0244e1ec20e90ce2eb6fc7e6ee22388
OleksandrKhrashchevskyi <o.khrashchevskyi@gmail.com>
2018-04-25 22:25:24 +0300

    add hm

---
+++ OleksandrKhrashchevskyi/22.04.18/Задачі (25 з 28)/22.04.18(1).php
@@ -0,0 +1,17 @@
+<?php
+// 1. Дан массив с элементами 'html', 'css', 'php', 'js', 'jq'. С помощью цикла
+// foreach выведите эти слова в столбик.</p>
+
+$a = array ('html','css','php','js', 'jq');
+
+foreach ($a as $a1)
+{
+    echo $a1. PHP_EOL;
+}
+
+
+
+
+
+
+
