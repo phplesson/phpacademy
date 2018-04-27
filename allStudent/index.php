b319f30f8ed3acb31634ec96d635969bdb5bb368
KogaiVladislav <dpylb818@gmail.com>
2018-04-27 14:43:03 +0300

    3 task done

--- allStudent/index.php
+++ allStudent/index.php
@@ -6,8 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="container">
+    <div class="sidebar">
 
+    </div>
+    <div class="content">
+
+    </div>
+</div>
 </body>
 </html>
