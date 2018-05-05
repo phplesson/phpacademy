f667b213ec1b7842d9d52ff290bb1edca2c5dda7
alexphpteacher <alexphpteacher@gmail.com>
2018-05-05 09:33:06 +0300

    prepare for the sixth lesson

---
+++ 06Lesson/04view/templates/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 05.05.18
+ * Time: 8:54
+ */
---
+++ 06Lesson/04view/templates/message.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 05.05.18
+ * Time: 9:07
+ */
