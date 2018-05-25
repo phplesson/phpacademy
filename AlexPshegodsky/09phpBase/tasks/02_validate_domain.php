bffec0d31e17b93b449f49920c0e3faec9742f56
Alex Pshegodsky <lexkiev@ukr.net>
2018-05-25 23:37:47 +0300

    regex tasks

--- AlexPshegodsky/09phpBase/tasks/02_validate_domain.php
+++ AlexPshegodsky/09phpBase/tasks/02_validate_domain.php
@@ -1,1 +1,21 @@
 <?php
+return [
+	'name' => 'Domain format validator',
+	'text' => 'Функция проверяет на корректность формата домена либо группы доменов (разделитель ",")',
+	'func' => function ($a) {
+		$result = array();
+		$domains = $a;
+		$pattern = '/^([a-z]([(\-)?a-z0-9]*[a-z0-9])*.)+(ru){1,2}/';
+		var_dump($domains);
+		if (!is_array($domains)){
+			$domains = explode(',', $domains);
+		}
+		
+		foreach ($domains as $domain) {
+			$result[$domain] = preg_match($pattern, $domain); 
+		}
+		return $result;
+	},
+	'inputType' => ['textarea' => 1,],
+	'view' => 'views/view02.php',
+];
