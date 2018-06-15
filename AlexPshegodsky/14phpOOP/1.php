1e3742d36fa0ade0b5d40b58010bf28756ad03cc
Alex Pshegodsky <lexkiev@ukr.net>
2018-06-16 02:22:29 +0300

    oop tasks 1-3;

---
+++ AlexPshegodsky/14phpOOP/1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 15.06.2018
+ * Time: 20:56
+ */
---
+++ AlexPshegodsky/14phpOOP/4.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 16.06.2018
+ * Time: 2:02
+ */
