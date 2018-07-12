c0928826f8391a6b5e0e365c9702581e5b62e2cc
Alex Pshegodsky <lexkiev@ukr.net>
2018-07-12 22:52:43 +0300

    minor bug fixes and code optimisation

--- AlexPshegodsky/14phpOOP/3.php
+++ AlexPshegodsky/14phpOOP/3.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 16.06.2018
- * Time: 1:55
- */
+
+class Human
+{
+	
+	protected $name;
+	protected $age;
+	
+	/**
+	 * Human constructor.
+	 * @param $name
+	 * @param $age
+	 */
+	public function __construct($name, $age)
+	{
+		$this->name = $name;
+		$this->age = $age;
+	}
+	
+	public function __toString()
+	{
+		return "Name: $this->name, Age: $this->age" . PHP_EOL;
+	}
+	
+	
+}
+
+$masha = new Human('Masha', '27');
+$pasha = new Human('Pasha', '11');
+
--- AlexPshegodsky/14phpOOP/5.php
+++ AlexPshegodsky/14phpOOP/5.php
@@ -1,7 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 16.06.2018
- * Time: 2:15
- */
+include "3.php";
+
+abstract class Registred extends Human
+{
+	protected $regDate;
+	
+	public function __construct($name, $age)
+	{
+		parent::__construct($name, $age);
+		$this->regDate = date('Y:d:m');
+		
+	}
+	
+	public function __toString()
+	{
+		return "Name: $this->name, Age: $this->age, Registred: $this->regDate" . PHP_EOL;
+	}
+	
+	public static function createObject($name,$age){
+		
+		return new self($name,$age);
+	}
+	
+}
+
+$registredHuman =
+
+echo $registredHuman;
